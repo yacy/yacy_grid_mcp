750e6bd54dc6224d4f5174c6c672fc94b46c05e4
Michael Peter Christen <mc@yacy.net>
2017-06-05 12:35:11 +0200

    removed unused import

--- src/main/java/net/yacy/grid/tools/MapUtil.java
+++ src/main/java/net/yacy/grid/tools/MapUtil.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
