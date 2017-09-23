b8de830540ffc89c98c0005c0b9b5432273c2d43
Michael Peter Christen <mc@yacy.net>
2017-09-23 08:33:03 +0200

    more control in mass update

--- bin/update_all.sh
+++ bin/update_all.sh
@@ -2,20 +2,24 @@
 cd "`dirname $0`"
 
 cd ..
+echo updating `pwd`
 git pull origin master
 gradle assemble
 
 cd ../yacy_grid_crawler
+echo updating `pwd`
 git submodule foreach git pull origin master
 git pull origin master
 gradle assemble
 
 cd ../yacy_grid_loader
+echo updating `pwd`
 git submodule foreach git pull origin master
 git pull origin master
 gradle assemble
 
 cd ../yacy_grid_parser
+echo updating `pwd`
 git submodule foreach git pull origin master
 git pull origin master
 gradle assemble
