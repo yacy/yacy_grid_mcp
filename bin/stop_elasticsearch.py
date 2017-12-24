3134b241de16a0db25c68d6d3ba7e2e0b94d6ef9
Michael Peter Christen <mc@yacy.net>
2017-12-24 13:13:11 +0100

    added python version of stop script for elasticsearch

---
+++ bin/stop_elasticsearch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import subprocess
+
+p = subprocess.Popen("ps aux | grep 'org.elasticsearch.bootstrap.Elasticsearch' | grep -v grep | awk '{print $2}' | cut -d/ -f 1", shell=True, stdout=subprocess.PIPE)
+pid = p.stdout.read()
+
+if pid:
+    print('killing elasticsearch, pid = ' + pid)
+    subprocess.call('kill ' + pid, shell=True)
+else:
+    print('elasticsearch was not running')
