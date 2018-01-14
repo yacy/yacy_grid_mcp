dee0cfbe27ac61f6546c8c11c201389d455d1283
Michael Peter Christen <mc@yacy.net>
2018-01-14 12:54:04 +0100

    fixed index deletion

--- bin/delete_index.sh
+++ bin/delete_index.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 cd "`dirname $0`"
 
-curl -XDELETE 'http://elastic:changeme@localhost:9200/web'
+# the following command is not good, because afterwards the index must be prepared with the mapping again
+#curl -XDELETE 'http://elastic:changeme@localhost:9200/web'
+
+curl -XPOST 'http://elastic:changeme@localhost:9200/web/_delete_by_query' -d '{
+    "query" : { 
+        "match_all" : {}
+    }
+}'
+
 echo
