2bd9d522464b9fe2f43857dea87a5efd77fb2b45
Michael Peter Christen <mc@yacy.net>
2018-03-06 00:29:46 +0100

    added mapping for crawler index

--- bin/prepare_index.sh
+++ bin/prepare_index.sh
@@ -2,4 +2,5 @@
 cd "`dirname $0`"
 cd ../conf/mappings
 curl -XPUT http://elastic:changeme@localhost:9200/web -H 'Content-Type: application/json' --data-binary "@web.json"
+curl -XPUT http://elastic:changeme@localhost:9200/crawler -H 'Content-Type: application/json' --data-binary "@crawler.json"
 echo
