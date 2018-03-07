e14bb6216de86be0cf289434bd5a5c816c5d7a15
Michael Peter Christen <mc@yacy.net>
2018-03-07 08:32:57 +0100

    added data access class for crawler index entries

---
+++ src/main/java/net/yacy/grid/io/index/CrawlerDocument.java
@@ -0,0 +1,119 @@
+/**
+ *  CrawlerDocument
+ *  Copyright 7.3.2018 by Michael Peter Christen, @0rb1t3r
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.yacy.grid.io.index;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import org.json.JSONObject;
+
+public class CrawlerDocument extends Document {
+
+    public static enum Status {
+        created,
+        loaded, load_failed,
+        parsed, parse_failed,
+        indexed
+    }
+    
+    public CrawlerDocument(Map<String, Object> map) {
+        super(map);
+    }
+    
+    public CrawlerDocument(JSONObject obj) {
+        super(obj);
+    }
+    
+    public static CrawlerDocument load(Index index, String id) throws IOException {
+        JSONObject json = index.query("crawler", "", id);
+        if (json == null) throw new IOException("no document with id " + id + " in index");
+        return new CrawlerDocument(json);
+    }
+    
+    public CrawlerDocument store(Index index, final String id, JSONObject object) throws IOException {
+        return this;
+    }
+
+    public CrawlerDocument setCrawlId(String crawlId) {
+        this.putString(CrawlerMapping.crawl_id_s, crawlId);
+        return this;
+    }
+    
+    public String getCrawlId() {
+        return this.getString(CrawlerMapping.crawl_id_s, "");
+    }
+    
+    public CrawlerDocument setURL(String url) {
+        this.putString(CrawlerMapping.url_s, url);
+        return this;
+    }
+    
+    public String getURL() {
+        return this.getString(CrawlerMapping.url_s, "");
+    }
+    
+    public CrawlerDocument setStatus(Status status) {
+        this.putString(CrawlerMapping.status_s, status.name());
+        return this;
+    }
+    
+    public Status getStatus() {
+        String status = this.getString(CrawlerMapping.status_s, "");
+        return Status.valueOf(status);
+    }
+    
+    public CrawlerDocument setInitDate(Date date) {
+        this.putDate(CrawlerMapping.init_date_dt, date);
+        return this;
+    }
+    
+    public Date getInitDate() {
+        return this.getDate(CrawlerMapping.init_date_dt);
+    }
+    
+    public CrawlerDocument setStatusDate(Date date) {
+        this.putDate(CrawlerMapping.status_date_dt, date);
+        return this;
+    }
+    
+    public Date getStatusDate() {
+        return this.getDate(CrawlerMapping.status_date_dt);
+    }
+    
+    public CrawlerDocument setCollections(List<String> collections) {
+        this.putStrings(CrawlerMapping.collection_sxt, collections);
+        return this;
+    }
+    
+    public List<String> getCollections() {
+        return this.getStrings(CrawlerMapping.collection_sxt);
+    }
+    
+    public CrawlerDocument setComment(String comment) {
+        this.putString(CrawlerMapping.comment_t, comment);
+        return this;
+    }
+    
+    public String getComment() {
+        return this.getString(CrawlerMapping.comment_t, "");
+    }
+    
+}
--- src/main/java/net/yacy/grid/io/index/Document.java
+++ src/main/java/net/yacy/grid/io/index/Document.java
@@ -40,71 +40,142 @@ public class Document extends JSONObject {
         super(obj.toMap());
     }
     
+    public Document putString(MappingDeclaration declaration, String s) {
+        if (!isString(declaration)) return this;
+        this.put(declaration.getMapping().name(), s);
+        return this;
+    }
+    
     public String getString(MappingDeclaration declaration, String dflt) {
-        MappingType type = declaration.getMapping().getType();
-        boolean valid = type == MappingType.string || type == MappingType.text_en_splitting_tight || type == MappingType.text_general;
-        valid = valid && !declaration.getMapping().isMultiValued();
-        assert valid; if (!valid) return null;
+        if (!isString(declaration)) return null;
         return this.optString(declaration.getMapping().name(), dflt);
     }
     
-    public List<String> getStrings(MappingDeclaration declaration) {
+    private boolean isString(MappingDeclaration declaration) {
         MappingType type = declaration.getMapping().getType();
         boolean valid = type == MappingType.string || type == MappingType.text_en_splitting_tight || type == MappingType.text_general;
-        valid = valid && declaration.getMapping().isMultiValued();
-        assert valid; if (!valid) return null;
+        valid = valid && !declaration.getMapping().isMultiValued();
+        assert valid;
+        return valid;
+    }
+
+    public Document putStrings(MappingDeclaration declaration, List<String> list) {
+        if (!isStrings(declaration)) return this;
+        this.put(CrawlerMapping.collection_sxt.getMapping().name(), list);
+        return this;
+    }
+
+    public List<String> getStrings(MappingDeclaration declaration) {
+        if (!isStrings(declaration)) return null;
         Object obj = this.opt(declaration.getMapping().name());
-        valid = obj instanceof JSONArray;
+        boolean valid = obj instanceof JSONArray;
         assert valid; if (!valid) return null;
         return ((JSONArray) obj).toList().stream().map(o -> Objects.toString(o, null)).collect(Collectors.toList());
     }
+
+    private boolean isStrings(MappingDeclaration declaration) {
+        MappingType type = declaration.getMapping().getType();
+        boolean valid = type == MappingType.string || type == MappingType.text_en_splitting_tight || type == MappingType.text_general;
+        valid = valid && declaration.getMapping().isMultiValued();
+        assert valid;
+        return valid;
+    }
+    
+    public Document putInt(MappingDeclaration declaration, int i) {
+        if (!isInt(declaration)) return this;
+        this.put(declaration.getMapping().name(), i);
+        return this;
+    }
     
     public int getInt(MappingDeclaration declaration) {
-        MappingType type = declaration.getMapping().getType();
-        boolean valid = type == MappingType.num_integer && !declaration.getMapping().isMultiValued();
-        assert valid;
-        if (!valid) return 0;
+        if (!isInt(declaration)) return 0;
         return this.optInt(declaration.getMapping().name());
     }
     
-    public List<Integer> getInts(MappingDeclaration declaration) {
+    private boolean isInt(MappingDeclaration declaration) {
         MappingType type = declaration.getMapping().getType();
-        boolean valid = type == MappingType.num_integer && declaration.getMapping().isMultiValued();
+        boolean valid = type == MappingType.num_integer && !declaration.getMapping().isMultiValued();
         assert valid;
-        if (!valid) return null;
+        return valid;
+    }
+    
+    public Document putInts(MappingDeclaration declaration, List<Integer> ints) {
+        if (!isInts(declaration)) return this;
+        this.put(declaration.getMapping().name(), ints);
+        return this;
+    }
+    
+    public List<Integer> getInts(MappingDeclaration declaration) {
+        if (!isInts(declaration)) return null;
         Object obj = this.opt(declaration.getMapping().name());
-        valid = obj instanceof JSONArray;
+        boolean valid = obj instanceof JSONArray;
         assert valid; if (!valid) return null;
         return ((JSONArray) obj).toList().stream().map(o -> (int) o).collect(Collectors.toList());
     }
-
-    public long getLong(MappingDeclaration declaration) {
+    
+    private boolean isInts(MappingDeclaration declaration) {
         MappingType type = declaration.getMapping().getType();
-        boolean valid = type == MappingType.num_long && !declaration.getMapping().isMultiValued();
+        boolean valid = type == MappingType.num_integer && declaration.getMapping().isMultiValued();
         assert valid;
-        if (!valid) return 0;
+        return valid;
+    }
+
+    public Document putLong(MappingDeclaration declaration, long l) {
+        if (!isLong(declaration)) return this;
+        this.put(declaration.getMapping().name(), l);
+        return this;
+    }
+    
+    public long getLong(MappingDeclaration declaration) {
+        if (!isLong(declaration)) return 0;
         return this.optLong(declaration.getMapping().name());
     }
     
-    public List<Long> getLongs(MappingDeclaration declaration) {
+    private boolean isLong(MappingDeclaration declaration) {
         MappingType type = declaration.getMapping().getType();
-        boolean valid = type == MappingType.num_long && declaration.getMapping().isMultiValued();
+        boolean valid = type == MappingType.num_long && !declaration.getMapping().isMultiValued();
         assert valid;
-        if (!valid) return null;
+        return valid;
+    }
+    
+    public Document putlongs(MappingDeclaration declaration, List<Long> longs) {
+        if (!isInts(declaration)) return this;
+        this.put(declaration.getMapping().name(), longs);
+        return this;
+    }
+    
+    public List<Long> getLongs(MappingDeclaration declaration) {
+        if (!isLongs(declaration)) return null;
         Object obj = this.opt(declaration.getMapping().name());
-        valid = obj instanceof JSONArray;
+        boolean valid = obj instanceof JSONArray;
         assert valid; if (!valid) return null;
         return ((JSONArray) obj).toList().stream().map(o -> (long) o).collect(Collectors.toList());
     }
+
+    private boolean isLongs(MappingDeclaration declaration) {
+        MappingType type = declaration.getMapping().getType();
+        boolean valid = type == MappingType.num_long && declaration.getMapping().isMultiValued();
+        assert valid;
+        return valid;
+    }
+
+    public Document putDate(MappingDeclaration declaration, Date date) {
+        if (!isDate(declaration)) return this;
+        this.put(declaration.getMapping().name(), DateParser.iso8601MillisFormat.format(date));
+        return this;
+    }
     
     public Date getDate(MappingDeclaration declaration) {
-        MappingType type = declaration.getMapping().getType();
-        boolean valid = type == MappingType.date && !declaration.getMapping().isMultiValued();
-        assert valid;
-        if (!valid) return null;
+       if (!isDate(declaration)) return null;
         if (!this.has(declaration.getMapping().name())) return null;
         String date = this.getString(declaration.getMapping().name());
         return DateParser.iso8601MillisParser(date);
     }
     
+    private boolean isDate(MappingDeclaration declaration) {
+        MappingType type = declaration.getMapping().getType();
+        boolean valid = type == MappingType.date && !declaration.getMapping().isMultiValued();
+        assert valid;
+        return valid;
+    }
 }
