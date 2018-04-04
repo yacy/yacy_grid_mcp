178b806390617f9240ecb31604184e352e6b82f2
Michael Peter Christen <mc@yacy.net>
2018-04-04 07:33:57 +0200

    delete a crawler document first before overwriting it

--- src/main/java/net/yacy/grid/io/index/CrawlerDocument.java
+++ src/main/java/net/yacy/grid/io/index/CrawlerDocument.java
@@ -54,6 +54,7 @@ public class CrawlerDocument extends Document {
     }
     
     public CrawlerDocument store(Index index, final String id) throws IOException {
+        index.delete("crawler", "event", id);
         index.add("crawler", "event", id, this);
         return this;
     }
