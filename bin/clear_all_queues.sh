ee91c89eaf1e0e55f361bd137e67219f33df329e
Michael Peter Christen <mc@yacy.net>
2018-04-06 07:42:16 +0200

    added enhanced clear scripts

--- bin/clear_all_queues.sh
+++ bin/clear_all_queues.sh
@@ -1,4 +1,5 @@
+./clear_crawler_queues.sh &
+./clear_parser_queues.sh &
+./clear_indexer_queues.sh &
 ./clear_loader_queues.sh
-curl 'http://127.0.0.1:8100/yacy/grid/mcp/messages/clear.json?serviceName=crawler&queueName=webcrawler'
-curl 'http://127.0.0.1:8100/yacy/grid/mcp/messages/clear.json?serviceName=indexer&queueName=elasticsearch'
-curl 'http://127.0.0.1:8100/yacy/grid/mcp/messages/clear.json?serviceName=parser&queueName=yacyparser'
+
---
+++ bin/clear_parser_queues.sh
@@ -0,0 +1,1 @@
+curl 'http://127.0.0.1:8100/yacy/grid/mcp/messages/clear.json?serviceName=parser&queueName=yacyparser_00'
