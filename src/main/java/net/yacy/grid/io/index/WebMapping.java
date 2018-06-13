1c0cc5003acae3e4f3baf67b4227a24444db7081
admin <admin@192.168.1.65>
2018-06-13 11:39:41 +0200

    added declaration of LSON-LD mapping type

--- src/main/java/net/yacy/grid/io/index/WebMapping.java
+++ src/main/java/net/yacy/grid/io/index/WebMapping.java
@@ -226,8 +226,9 @@ public enum WebMapping implements MappingDeclaration {
     ext_tracker_val(MappingType.num_integer, true, true, true, false, false, "number of attribute counts in ext_tracker_txt"),
     ext_title_txt(MappingType.text_general, true, true, true, false, false, "names matching title expressions"),
     ext_title_val(MappingType.num_integer, true, true, true, false, false, "number of matching title expressions"),
-    vocabularies_sxt(MappingType.string, true, true, true, false, false, "collection of all vocabulary names that have a matcher in the document - use this to boost with vocabularies");
-    
+    vocabularies_sxt(MappingType.string, true, true, true, false, false, "collection of all vocabulary names that have a matcher in the document - use this to boost with vocabularies"),
+    ld_o(MappingType.object, false, true, false, false, false, "JSON-LD object with node object");
+   
     public final static String CORE_NAME = "collection1"; // this was the default core name up to Solr 4.4.0. This default name was stored in CoreContainer.DEFAULT_DEFAULT_CORE_NAME but was removed in Solr 4.5.0
 
     public final static String VOCABULARY_PREFIX = "vocabulary_"; // collects all terms that appear for each vocabulary
