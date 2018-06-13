34fb12a64cc3dd9632768947f60e0c17fc9503c6
Michael Christen <mc@yacy.net>
2018-06-13 19:10:26 +0200

    added stop shell script which uses latest pid and kill files

---
+++ bin/stop.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+cd "`dirname $0`"
+cd ../data
+KILLFILE="mcp-8100.kill"
+PIDFILE="mcp-8100.pid"
+
+# first method to terminate the process
+if [ -f "$KILLFILE" ];
+then
+   rm $KILLFILE
+   echo "termination requested, waiting.."
+   # this can take 10 seconds..
+   sleep 10
+fi
+
+# second method to terminate the process
+if [ -f "$PIDFILE" ];
+then
+   fuser -k $PIDFILE
+fi
+
+# check if file does not exist any more which would be a sign that this has terminated
+if [ ! -f "$PIDFILE" ];
+then
+   echo "process terminated"
+fi
+
