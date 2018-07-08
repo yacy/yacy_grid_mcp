8e2540be6f6ed270c4141bc1bdf15eac3dd44c12
Michael Christen <mc@yacy.net>
2018-07-08 10:42:23 +0200

    removed unused methods

--- src/main/java/ai/susi/mind/SusiAction.java
+++ src/main/java/ai/susi/mind/SusiAction.java
@@ -20,9 +20,6 @@
 package ai.susi.mind;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collection;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -36,17 +33,6 @@ import net.yacy.grid.tools.JSONList;
  * we want to visualize deduces data as a graph or in a picture, thats an action.
  */
 public class SusiAction {
-
-    public static enum RenderType {answer, table, piechart, rss, self, websearch, anchor, map, loader, parser, indexer;}
-    public static enum SelectionType {random, roundrobin;}
-    public static enum DialogType {
-        answer,    // a sentence which may end a conversation
-        question,  // a sentence which may cause that the user answers with a fact
-        reply;     // a response of an answers of the user from a question aked by sudy
-        public int getSubscore() {
-            return this.ordinal();
-        }
-    }
     
     private JSONObject json;
 
@@ -58,68 +44,6 @@ public class SusiAction {
         this.json = json;
     }
 
-    public static JSONObject answerAction(String[] answers) {
-        JSONObject json = new JSONObject();
-        JSONArray phrases = new JSONArray();
-        json.put("type", RenderType.answer.name());
-        json.put("select", SelectionType.random.name());
-        json.put("phrases", phrases);
-        for (String answer: answers) phrases.put(answer.trim());
-        return json;
-    }
-    
-    /**
-     * Get the render type. That can be used to filter specific information from the action JSON object
-     * to create specific activities like 'saying' a sentence, painting a graph and so on.
-     * @return the action type
-     */
-    public RenderType getRenderType() {
-        if (renderTypeCache == null) 
-            renderTypeCache = this.json.has("type") ? RenderType.valueOf(this.json.getString("type")) : null;
-        return renderTypeCache;
-    }
-    private RenderType renderTypeCache = null;
-
-    public DialogType getDialogType() {
-        if (this.getRenderType() != RenderType.answer) return DialogType.answer;
-        return getDialogType(getPhrases());
-    }
-    
-    public static DialogType getDialogType(Collection<String> phrases) {
-        DialogType type = DialogType.reply;
-        for (String phrase: phrases) {
-            DialogType t = getDialogType(phrase);
-            if (t.getSubscore() < type.getSubscore()) type = t;
-        }
-        return type;
-    }
-    
-    public static DialogType getDialogType(String phrase) {
-        if (phrase.indexOf('?') > 3) { // the question mark must not be at the beginning
-            return phrase.indexOf(". ") >= 0 ? DialogType.reply : DialogType.question;
-        }
-        return DialogType.answer;
-    }
-    
-    /**
-     * If the action involves the reproduction of phrases (=strings) then they can be retrieved here
-     * @return the action phrases
-     */
-    public ArrayList<String> getPhrases() {
-        if (phrasesCache == null) {
-            ArrayList<String> a = new ArrayList<>();
-            // actions may have either a single expression "expression" or a phrases object with 
-            if (this.json.has("expression")) {
-                a.add(this.json.getString("expression"));
-            } else if (this.json.has("phrases")) {
-                this.json.getJSONArray("phrases").forEach(p -> a.add((String) p));
-            } else return null;
-            phrasesCache = a;
-        }
-        return phrasesCache;
-    }
-    private ArrayList<String> phrasesCache = null;
-    
     /**
      * if the action contains more String attributes where these strings are named, they can be retrieved here
      * @param attr the name of the string attribute
@@ -127,27 +51,6 @@ public class SusiAction {
      */
     public String getStringAttr(String attr) {
         return this.json.has(attr) ? this.json.getString(attr) : "";
-    }
-    public boolean getBooleanAttr(String attr) {
-        return this.json.has(attr) ? this.json.getBoolean(attr) : false;
-    }
-    public long getLongAttr(String attr) {
-        return this.json.has(attr) ? this.json.getLong(attr) : -1;
-    }
-    public double getDoubleAttr(String attr) {
-        return this.json.has(attr) ? this.json.getDouble(attr) : Double.NaN;
-    }
-    public JSONArray getArrayAttr(String attr) {
-        return this.json.has(attr) ? this.json.getJSONArray(attr) : new JSONArray();
-    }
-    
-    /**
-     * If the action contains integer attributes, they can be retrieved here
-     * @param attr the name of the integer attribute
-     * @return the integer number
-     */
-    public int getIntAttr(String attr) {
-        return this.json.has(attr) ? this.json.getInt(attr) : 0;
     }
 
     /**
@@ -164,48 +67,10 @@ public class SusiAction {
         return j;
     }
     
-    /**
-     * actions may have actions embedded, which act as follow-up actions.
-     * @return the action inside that action, not a clone!
-     */
-    public JSONArray getEmbeddedActions() {
-        return this.json.getJSONArray("actions");
-    }
-
     public boolean hasAsset(String name) {
         if (!this.json.has("assets")) return false;
         JSONObject assets = this.json.getJSONObject("assets");
         return assets.has(name);
-    }
-    
-    // attach a binary asset to the action
-    public SusiAction setBinaryAsset(String name, byte[] b) {
-        JSONObject assets;
-        if (this.json.has("assets")) assets = this.json.getJSONObject("assets"); else {
-        	assets = new JSONObject();
-            this.json.put("assets", assets);
-        }
-        final String bAsBase64 = Base64.getEncoder().encodeToString(b);
-        assets.put(name, bAsBase64);
-        return this;
-    }
-    
-    // read a binary asset from the action
-    public byte[] getBinaryAsset(String name) {
-        if (!this.json.has("assets")) return null;
-        JSONObject assets = this.json.getJSONObject("assets");
-        String bAsBase64 = assets.getString(name);
-        return Base64.getDecoder().decode(bAsBase64);
-    }
-
-    public SusiAction setJSONListAsset(String name, JSONList list) {
-    	JSONObject assets;
-        if (this.json.has("assets")) assets = this.json.getJSONObject("assets"); else {
-        	assets = new JSONObject();
-            this.json.put("assets", assets);
-        }
-        assets.put(name, list.toArray());
-    	return this;
     }
     
     public JSONList getJSONListAsset(String name) {
