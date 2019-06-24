03c54b304ff4822bcf40ba7b8f4a641c2e08d5e8
Michael Peter Christen <mc@yacy.net>
2019-06-24 18:11:05 +0200

    added elasticsearch address hint

--- bin/start_elasticsearch.py
+++ bin/start_elasticsearch.py
@@ -46,6 +46,7 @@ else:
         while not checkportopen(9200):
             print('waiting until elasticsearch is running...')
             time.sleep(3)
+    print('elasticsearch is running at http://127.0.0.1:9200/')
         #print('preparing index...')
         #os.chdir(path_apphome)
         #subprocess.call('bin/prepare_index.sh', shell=True)
