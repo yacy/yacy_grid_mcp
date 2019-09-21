c66bc3c4de25e2be6ea6f8a823869f29415bf0eb
Michael Christen <mc@acy.net>
2019-09-21 21:21:39 +0300

    using local gradlew instead of os-gradle in build tool

--- bin/update_all.sh
+++ bin/update_all.sh
@@ -4,28 +4,28 @@ cd "`dirname $0`"
 cd ..
 echo updating `pwd`
 git pull origin master
-gradle assemble
+./gradlew assemble
 
 cd ../yacy_grid_crawler
 echo updating `pwd`
 git submodule foreach git pull origin master
 git pull origin master
-gradle assemble
+./gradlew assemble
 
 cd ../yacy_grid_loader
 echo updating `pwd`
 git submodule foreach git pull origin master
 git pull origin master
-gradle assemble
+./gradlew assemble
 
 cd ../yacy_grid_parser
 echo updating `pwd`
 git submodule foreach git pull origin master
 git pull origin master
-gradle assemble
+./gradlew assemble
 
 cd ../yacy_grid_search
 echo updating `pwd`
 git submodule foreach git pull origin master
 git pull origin master
-gradle assemble
+./gradlew assemble
