f892cb6f59ca0e00b45c695da750f1126c874c26
Michael Peter Christen <mc@yacy.net>
2021-12-16 00:29:25 +0100

    changed hazelcast initialization time to be able to use hazelcast in other grid components properly

--- src/main/java/net/yacy/grid/mcp/Service.java
+++ src/main/java/net/yacy/grid/mcp/Service.java
@@ -6,12 +6,12 @@
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- *  
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program in the file lgpl21.txt
  *  If not, see <http://www.gnu.org/licenses/>.
@@ -107,6 +107,12 @@ public enum Service {
         // find data path
         int port = Integer.parseInt(config.get("port"));
         Data.init(dataInstancePath(data_dir, port), config, localStorage);
+
+        // start hazelcast service
+        Config hazelcastconfig = new Config().setClusterName("YaCyGrid").setInstanceName(Service.type.name());
+        hazelcast = Hazelcast.newHazelcastInstance(hazelcastconfig);
+        String uuid = hazelcast.getCluster().getLocalMember().getUuid().toString();
+        hazelcast.getMap("status").put(uuid, StatusService.status());
     }
 
     /**
@@ -149,12 +155,6 @@ public enum Service {
 
             // give positive feedback
             Data.logger.info("Service started at port " + port);
-
-            // start hazelcast service
-            Config config = new Config().setClusterName("YaCyGrid").setInstanceName(Service.type.name());
-            hazelcast = Hazelcast.newHazelcastInstance(config);
-            String uuid = hazelcast.getCluster().getLocalMember().getUuid().toString();
-            hazelcast.getMap("status").put(uuid, StatusService.status());
 
             // prepare shutdown signal
             boolean pidkillfileCreated = false;
