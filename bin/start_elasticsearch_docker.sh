dc2f093df2eed3f00478ae1fd2f2e56e36c54091
Michael Peter Christen <mc@yacy.net>
2022-05-02 22:50:03 +0200

    renaming elasticsearch volume for docker for consistency with rabbitmq and minio

--- bin/start_elasticsearch_docker.sh
+++ bin/start_elasticsearch_docker.sh
@@ -9,7 +9,7 @@ if [ ${containerRuns} -gt 0 ]; then
 elif [ ${containerExists} -gt 0 ]; then
   docker start yacy-grid-elasticsearch
 else
-  docker run -d --restart=unless-stopped --name yacy-grid-elasticsearch -p 127.0.0.1:9200:9200 -p 127.0.0.1:9300:9300 -v `pwd`/../conf/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -v yacy-grid-elasticsearch-data:/usr/share/elasticsearch/data elasticsearch:6.8.20
+  docker run -d --restart=unless-stopped --name yacy-grid-elasticsearch -p 127.0.0.1:9200:9200 -p 127.0.0.1:9300:9300 -v `pwd`/../conf/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -v yacy-grid-elasticsearch:/usr/share/elasticsearch/data elasticsearch:6.8.20
 fi
 echo "Elasticsearch started. Open http://127.0.0.1:9200/_cat/health to check health status"
 
