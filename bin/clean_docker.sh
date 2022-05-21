c9935e38ac1ec5a5c4ce8c076b47700bcd59ed85
Michael Peter Christen <mc@yacy.net>
2022-05-21 10:13:43 +0200

    added grafana and elastichq

--- bin/clean_docker.sh
+++ bin/clean_docker.sh
@@ -10,8 +10,11 @@ docker rm yacy-grid-rabbitmq
 docker stop yacy-grid-minio
 docker rm yacy-grid-minio
 
-docker stop yacy-grid-elasticsearch
-docker rm yacy-grid-elasticsearch
+docker stop yacy-grid-elastichq
+docker rm yacy-grid-elastichq
+
+docker stop yacy-grid-grafana
+docker rm yacy-grid-grafana
 
 docker stop yacy-grid-mcp
 docker rm yacy-grid-mcp
